76542ca600915fcdbd2664fd67d3c45519c03304
2014CAIS01 <zbszx040504@126.com>
2017-05-07 23:35:03 +0800

    UVaOJ

---
+++ UVaOJ/Ch3/3-10.cpp
@@ -0,0 +1,59 @@
+// Box, ACM/ICPC NEERC 2004, UVa1587
+
+/*Sample Input
+1345 2584
+2584 683
+2584 1345
+683 1345
+683 1345
+2584 683
+1234 4567
+1234 4567
+4567 4321
+4322 4567
+4321 1234
+4321 1234
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	const int n = 6;
+	double a[6], b[6];
+	while (cin >> a[0] >> b[0])
+	{
+		for (int i = 1; i < n; i++)
+			cin >> a[i] >> b[i];
+		for (int i = 0; i < n; i++)
+			if (a[i] > b[i])
+				swap(a[i], b[i]);
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n - i - 1; j++)
+				if (a[j] > a[j + 1])
+				{
+					swap(a[j], a[j + 1]);
+					swap(b[j], b[j + 1]);
+				}
+		for (int i = 0; i < 4; i++)
+			for (int j = 0; j < 4 - i - 1; j++)
+				if (b[j] > b[j + 1])
+				{
+					swap(a[j], a[j + 1]);
+					swap(b[j], b[j + 1]);
+				}
+		int flag = 0;
+		/*for (int i = 0; i < n; i++)
+			cout << a[i] << " " << b[i] << endl;*/
+		if (a[0] == a[1] && a[2] == a[3] && a[0] == a[2])
+			if (b[0] == b[1] && a[4] == a[5] && b[0] == a[4])
+				if (b[2] == b[3] && b[4] == b[5] && b[2] == b[4])
+					flag = 1;
+		if (flag)
+			cout << "POSSIBLE" << endl;
+		else
+			cout << "IMPOSSIBLE" << endl;
+	}
+	return 0;
+}
---
+++ UVaOJ/Ch3/3-9.cpp
@@ -0,0 +1,33 @@
+// All in All, UVa 10340
+
+/*Sample Input
+sequence subsequence
+person compression
+VERDI vivaVittorioEmanueleReDiItalia
+caseDoesMatter CaseDoesMatter
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	string s;
+	string t;
+	while (cin >> s >> t)
+	{
+		int i = 0, j = 0;
+		while (i < s.length() && j < t.length())
+		{
+			if (s[i] == t[j])
+				i++, j++;
+			else
+				j++;
+		}
+		if (s.length() == i)
+			cout << "Yes" << endl;
+		else
+			cout << "No" << endl;
+	}
+	return 0;
+}
