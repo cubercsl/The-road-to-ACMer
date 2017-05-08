e86c99148bee65e8f79910571defdc76da620d52
2014CAIS01 <zbszx040504@126.com>
2017-05-08 23:34:57 +0800

    Codeforces

---
+++ Codeforces/Round #412 (Div. 2)/807C.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int x, y, p, q;
+		long long a, b, mid, l = 0, r = 1e9, ans = -1;
+		cin >> x >> y >> p >> q;
+		while (l <= r)
+		{
+			mid = (l + r) / 2;
+			a = mid * p - x, b = mid * q - y;
+			if (a >= 0 && b >= 0 && a <= b)
+			{
+				ans = mid;
+				r = mid - 1;
+			}
+			else
+				l = mid + 1;
+		}
+		if (ans == -1)
+			cout << -1 << endl;
+		else
+			cout << ans * q - y << endl;
+	}
+	return 0;
+}
---
+++ Codeforces/Round #412 (Div. 2)/807D.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int a[120][5];
+int solve[5];
+int cheat[5];
+
+int cal(int time, int ac, int total)
+{
+	if (time == -1)
+		return 0;
+	if (2 * ac > total)
+		return 2 * (250 - time);
+	if (4 * ac > total)
+		return 4 * (250 - time);
+	if (8 * ac > total)
+		return 6 * (250 - time);
+	if (16 * ac > total)
+		return 8 * (250 - time);
+	if (32 * ac > total)
+		return 10 * (250 - time);
+	return 12 * (250 - time);
+}
+
+bool check(int k)
+{
+	for (int i = 0; i < 5; i++)
+		cheat[i] = solve[i];
+	for (int i = 0; i < 5; i++)
+		if (a[0][i] != -1)
+		{
+			if (a[1][i] == -1)
+				continue;
+			else if (a[0][i] > a[1][i])
+				cheat[i] += k;
+		}
+	int vasya = 0, petya = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		vasya += cal(a[0][i], cheat[i], n + k);
+		petya += cal(a[1][i], cheat[i], n + k);
+	}
+	return vasya > petya;
+}
+
+int main()
+{
+	bool flag = 0;
+	while (cin >> n)
+	{
+		memset(a, 0, sizeof(a[0][0]) * 120 * 5);
+		memset(solve, 0, sizeof(solve));
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < 5; j++)
+			{
+				cin >> a[i][j];
+				if (a[i][j] != -1)
+					solve[j]++;
+			}
+		for (int i = 0; i < 33 * n; i++)
+			if (check(i))
+			{
+				cout << i << endl;
+				flag = 1;
+				break;
+			}
+		if (!flag)
+			cout << -1 << endl;
+	}
+	return 0;
+}
