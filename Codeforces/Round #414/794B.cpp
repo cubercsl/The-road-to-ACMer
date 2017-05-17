5090188595a044cd506173c03eb6dbc8db5dae75
2014CAIS01 <zbszx040504@126.com>
2017-05-17 23:05:41 +0800

    Codeforces

---
+++ Codeforces/Round #414/794B.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n, h;
+	double s;
+
+	while (cin >> n >> h)
+	{
+		s = 0.5 * h;
+		s /= 2.0;
+		double t = n;
+		s /= t;
+		for (int i = 1; i <= n - 1; i++)
+		{
+			if (i != 1)
+				cout << " ";
+			cout << fixed << setprecision(11) << 2 * sqrt(h * i * s);
+		}
+		cout << endl;
+	}
+	return 0;
+}
