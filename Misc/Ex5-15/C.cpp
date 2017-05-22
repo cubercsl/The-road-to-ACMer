b738f788d33b98937fcad6830a8ce8a84740ee32
2014CAIS01 <zbszx040504@126.com>
2017-05-22 19:41:15 +0800

    Update

---
+++ Misc/Ex5-15/C.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        if (n & (n + 1))
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
