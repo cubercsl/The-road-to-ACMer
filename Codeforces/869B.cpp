0f174fe7e379c0632468b1df8734efabc39add4e
2014CAIS01 <zbszx040504@126.com>
2017-10-07 20:56:17 +0800

    Codeforces

---
+++ Codeforces/869B.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define clr(a, x) memset(a, x, sizeof(a))
+#define mp(x, y) make_pair(x, y)
+#define pb(x) push_back(x)
+#define X first
+#define Y second
+#define fastin                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+const int INF = 0x3f3f3f3f;
+const int mod = 1e9 + 7;
+const double eps = 1e-6;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("1.in", "r", stdin);
+    freopen("1.out", "w", stdout);
+#endif
+    ll a, b;
+    while (cin >> a >> b)
+    {
+        ll ans = 1;
+        for (ll i = a + 1; i <= b; i++)
+        {
+            ans = (ans * (i % 10)) % 10;
+            if (ans == 0) break;
+        }
+        cout << ans << endl;
+    }
+
+    return 0;
+}
