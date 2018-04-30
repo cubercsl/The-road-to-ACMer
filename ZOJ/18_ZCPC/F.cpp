1cde08cd82d58d353c64e36b0d7a5910c4b0ff71
2014CAIS01 <zbszx040504@126.com>
2018-04-30 17:21:43 +0800

    ZOJ

--- ZOJ/18_ZCPC/F.cpp
+++ ZOJ/18_ZCPC/F.cpp
@@ -13,8 +13,10 @@ typedef long double ld;
 typedef pair<int, int> PII;
 typedef vector<int> VI;
 const int INF = 0x3f3f3f3f;
-const int mod = 1e9 + 7;
+// const int mod = 1e9 + 7;
 const double eps = 1e-6;
+
+const int mod = 1e9;
 
 int main()
 {
@@ -22,6 +24,33 @@ int main()
     freopen("1.in", "r", stdin);
     freopen("1.out", "w", stdout);
 #endif
-    
+    fastin int T;
+    cin >> T;
+    while (T--)
+    {
+        int n, m;
+        cin >> n >> m;
+        vector<int> a(n + 1);
+        for (int i = 1; i <= n; i++) cin >> a[i];
+        sort(a.begin(), a.end());
+        vector<vector<ll> > s(32, vector<ll>(n + 1));
+        for (int i = 1; i < 30; i++)
+            for (int j = 1; j <= n; j++) s[i][j] = s[i][j - 1] + a[j] / i;
+        ll ans = 0;
+        for (int i = 1, p; i <= m; i++)
+        {
+            cin >> p;
+            ll b = p;
+            int l = 0, c = 1;
+            while (l < n)
+            {
+                int r = upper_bound(a.begin(), a.end(), b) - a.begin() - 1;
+                (ans += 1LL * (s[c][r] - s[c][l]) * i) %= mod;
+                c++, b *= p;
+                l = r;
+            }
+        }
+        cout << ans << endl;
+    }
     return 0;
 }
