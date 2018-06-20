7c63c21fd4f6522f067f03bd1af204a7d4a7168b
2014CAIS01 <zbszx040504@126.com>
2018-06-21 07:06:55 +0800

    update

--- Summer2018/Personal02/B.cpp
+++ Summer2018/Personal02/B.cpp
@@ -1,20 +1,122 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define clr(a, x) memset(a, x, sizeof(a))
-#define mp(x, y) make_pair(x, y)
-#define pb(x) push_back(x)
-#define X first
-#define Y second
-#define fastin                    \
-    ios_base::sync_with_stdio(0); \
-    cin.tie(0);
+
+#pragma optimize("-O3")
+
 typedef long long ll;
 typedef long double ld;
-typedef pair<int, int> PII;
 typedef vector<int> VI;
-const int INF = 0x3f3f3f3f;
-const int mod = 1e9 + 7;
-const double eps = 1e-6;
+typedef vector<ll> VL;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+
+string to_string(string s) { return '"' + s + '"'; }
+
+string to_string(const char* s) { return to_string((string)s); }
+
+string to_string(const bool& b) { return (b ? "true" : "false"); }
+
+template <typename A, typename B>
+string to_string(pair<A, B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+
+template <typename A>
+string to_string(const A& v)
+{
+    bool first = true;
+    string res = "{";
+    for (const auto& x : v)
+    {
+        if (!first)
+            res += ", ";
+        first = false;
+        res += to_string(x);
+    }
+    res += "}";
+    return res;
+}
+
+void debug_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T)
+{
+    cerr << " " << to_string(H);
+    debug_out(T...);
+}
+
+#ifndef ONLINE_JUDGE
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__);
+#else
+#define debug(...) 42;
+#define cerr \
+    if (false) cout
+#endif
+
+template <typename T>
+inline void _read(T& x)
+{
+    cin >> x;
+}
+
+template <typename A, typename B>
+inline void _read(pair<A, B>& x)
+{
+    _read(x.first);
+    _read(x.second);
+}
+
+template <typename T>
+inline void _read(vector<T>& x)
+{
+    for (auto& v : x)
+        _read(v);
+}
+
+void R() {}
+template <typename T, typename... U>
+void R(T& head, U&... tail)
+{
+    _read(head);
+    R(tail...);
+}
+
+#define endl '\n'
+
+template <typename T>
+inline void _write(const T& x)
+{
+    cout << x << ' ';
+}
+
+template <typename A, typename B>
+inline void _write(const pair<A, B>& x)
+{
+    _write(x.first);
+    _write(x.second);
+}
+
+template <typename T>
+inline void _write(const vector<T>& in)
+{
+    for (const auto& x : in)
+        _write(x);
+}
+
+void W() { cout << endl; }
+
+template <typename T, typename... U>
+void W(const T& head, const U&... tail)
+{
+    _write(head);
+    W(tail...);
+}
+
+#define my_sort_unique(c) (sort(c.begin(), c.end()), c.resize(distance(c.begin(), unique(c.begin(), c.end()))))
+#define my_unique(a) a.resize(distance(a.begin(), unique(a.begin(), a.end())))
+#define X first
+#define Y second
+
+void go();
 
 int main()
 {
@@ -22,6 +124,68 @@ int main()
     freopen("1.in", "r", stdin);
     freopen("1.out", "w", stdout);
 #endif
-    
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    go();
     return 0;
+}
+
+/****************************************************************************************************/
+
+const int mod = 1e9 + 7;
+const int N = 1 << 20;
+
+void go()
+{
+    vector<VI> C(600, VI());
+    for (int i = 0; i < 600; i++)
+    {
+        C[i].resize(i + 1);
+        C[i][0] = C[i][i] = 1;
+        for (int j = 1; j < i; j++) C[i][j] = (C[i - 1][j] + C[i - 1][j - 1]) % mod;
+    }
+    VI p, vis(N);
+    for (int i = 2; i < N; i++)
+    {
+        if (!vis[i]) p.push_back(i);
+        for (auto& j : p)
+        {
+            if (i * j > N) break;
+            vis[i * j] = 1;
+            if (i % j == 0) break;
+        }
+    }
+    int n;
+    while (cin >> n)
+    {
+        map<int, int> div;
+        for (int i = 0, x; i < n; i++)
+        {
+            cin >> x;
+            for (auto& t : p)
+            {
+                if (t * t > x) break;
+                while (x % t == 0) div[t]++, x /= t;
+            }
+            if (x != 1) div[x]++;
+        }
+        debug(div);
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int tmp = C[n][i];
+            for (auto& d : div)
+            {
+                int& m = d.Y;
+                tmp = 1LL * C[m + n - i - 1][n - i - 1] * tmp % mod;
+            }
+            if (i & 1)
+                ans = (ans - tmp + mod) % mod;
+            else
+                ans = (ans + tmp) % mod;
+            debug(tmp, ans);
+        }
+        cout << ans << endl;
+    }
 }
